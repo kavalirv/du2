8d90150bb8eb81c87bebd80230c50d16d710ae85
Václav Kavalír <kavalirv@natur.cuni.cz>
2019-12-13 17:31:31 +0100

    funkcni sort

--- quadtree.py
+++ quadtree.py
@@ -1,4 +1,5 @@
-def qd_tree_x(points):
+
+def qd_tree_complete(points):
     if len(points) <= 1:
         print(points)
         return points
@@ -6,12 +7,18 @@ def qd_tree_x(points):
     right = len(points)
     mid = int((left+right-1)//2)  # -1 řeší dělění lichých čísel
 
-    points.sort(key=lambda p: [0])
-    lefty = points[:mid]
-    righty = points[mid:]
-    middle = lefty[-1]
-    print("Middle point: {}, body před: {}, body za: {}".format(middle, lefty, righty))
+    #  sort podle x
+    points.sort(key=lambda p: p[0])
+    lefty_x = points[:mid]
+    righty_x = points[mid:]
+    middle_x = lefty_x[-1]
+    print("Middle point: {}, body před: {}, body za: {}".format(middle_x, lefty_x, righty_x))
 
-qd_tree_x([(0.2721733396134992, 0.16110421582040624), (0.5828583727174798, 0.14221065549839018), (0.7408569558116688, 0.836419046573464), (0.3299483355463435, 0.12215068526416473), (0.5610962373889725, 0.3706195830443474)])
+    #  sort podle y
+    points.sort(key=lambda p: p[1])
+    lefty_y = points[:mid]
+    righty_y = points[mid:]
+    middle_y = lefty_y[-1]
+    print("Middle point: {}, body před: {}, body za: {}".format(middle_y, lefty_y, righty_y))
 
-def qd_tree_y(points):
+qd_tree_complete([(0.2721733396134992, 0.16110421582040624), (0.5828583727174798, 0.14221065549839018), (0.7408569558116688, 0.836419046573464), (0.3299483355463435, 0.12215068526416473), (0.5610962373889725, 0.3706195830443474)])
